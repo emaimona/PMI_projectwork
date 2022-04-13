fd651f5c42cd05cd8a0650c8ce4f4ac680e29886
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-04-13 18:57:13 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import splash.Splash;
+
+public class Main {
+    public static void main(String[] args) {
+        Splash n = new Splash();
+        n.main();
+        System.out.println();
+    }
+}
---
+++ src/main/java/splash/Splash.java
@@ -0,0 +1,17 @@
+package splash;
+public class Splash {
+
+    public void header(String area){
+        System.out.println("\t\t\t*** SCHOOL MANAGMENT SYSTEM ***\n");
+        System.out.println("===> "+ area + "\n\n");
+    }
+
+    public void login(){
+
+    }
+
+    public void main() {
+        header("Login");
+        System.out.println("Please ");
+    }
+}
